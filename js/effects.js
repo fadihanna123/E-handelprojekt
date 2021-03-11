061f1234cf2f4b81796088e8ae26bc2ba423854e
Fadi Hanna <54924938+fadihanna123@users.noreply.github.com>
2021-03-11 22:13:03 +0100

    Edit typescript types

--- js/effects.js
+++ js/effects.js
@@ -1,13 +1,4 @@
 "use strict";
 const a = document.getElementById("korg");
-const show = () => {
-    $("#searchtext").fadeToggle(2000);
-};
-const showvaru = () => {
-    $("#varu").show(1000);
-};
-const hidevaru = () => {
-    $("#varu").hide(1000);
-};
-a.addEventListener("mouseover", showvaru(), false);
-a.addEventListener("mouseout", hidevaru(), false);
+a.addEventListener("mouseover", () => $("#searchtext").fadeToggle(2000), false);
+a.addEventListener("mouseout", () => $("#varu").show(1000), false);
--- js/effects.ts
+++ js/effects.ts
@@ -1,16 +1,4 @@
 const a = document.getElementById("korg")! as HTMLLinkElement;
 
-const show = () => {
-  $("#searchtext").fadeToggle(2000);
-};
-
-const showvaru = (): any => {
-  $("#varu").show(1000);
-};
-
-const hidevaru = (): any => {
-  $("#varu").hide(1000);
-};
-
-a.addEventListener("mouseover", showvaru(), false);
-a.addEventListener("mouseout", hidevaru(), false);
+a.addEventListener("mouseover", () => $("#searchtext").fadeToggle(2000), false);
+a.addEventListener("mouseout", () => $("#varu").show(1000), false);
