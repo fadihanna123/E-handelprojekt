0f3433a6825fb5d39e297029c92c8e07f2d9add6
Fadi Hanna <fadialias230@gmail.com>
2024-09-23 00:53:44 +0200

    Add Stylelint, Prettier, Eslint, add tsconfig rules, remove scss folder

--- js/effects.js
+++ js/effects.js
@@ -1,4 +1,5 @@
 "use strict";
-const a = document.getElementById("korg");
-a.addEventListener("mouseover", () => $("#searchtext").fadeToggle(2000), false);
-a.addEventListener("mouseout", () => $("#varu").show(1000), false);
+// @ts-check
+const a = document.getElementById('korg');
+a.addEventListener('mouseover', () => $('#searchtext').fadeToggle(2000), false);
+a.addEventListener('mouseout', () => $('#varu').show(1000), false);
--- ts/effects.ts
+++ ts/effects.ts
@@ -1,4 +1,5 @@
-const a = document.getElementById("korg")! as HTMLLinkElement;
+// @ts-check
+const a = document.getElementById('korg')! as HTMLLinkElement;
 
-a.addEventListener("mouseover", () => $("#searchtext").fadeToggle(2000), false);
-a.addEventListener("mouseout", () => $("#varu").show(1000), false);
+a.addEventListener('mouseover', () => $('#searchtext').fadeToggle(2000), false);
+a.addEventListener('mouseout', () => $('#varu').show(1000), false);
