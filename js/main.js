957179bea59398166449a39cac01e8c9b8aaca8f
Fadi Hanna <fadialias230@gmail.com>
2024-09-24 09:15:17 +0200

    Minify js code, upgrade packages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,1 @@
-'use strict';
-const showvaru = () => $('#searchtext').fadeToggle(2e3),
-  hidevaru = () => $('#varu').show(1e3);
+"use strict";const showvaru=()=>$("#searchtext").fadeToggle(2e3),hidevaru=()=>$("#varu").show(1e3);
